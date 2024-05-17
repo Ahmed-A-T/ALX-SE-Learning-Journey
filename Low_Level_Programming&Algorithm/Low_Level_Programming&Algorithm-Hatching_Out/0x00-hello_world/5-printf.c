4c6b51e852e387423f4e99cc071ccea92a584040
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-05-17 14:53:51 +0300

    Complete Task 5

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-printf.c
+ * Author: Ahmed-A-T
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
