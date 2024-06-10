42a5fbf924a68b6bd7aabf7d9219188963fefc79
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-06-10 21:47:21 +0300

    Complete Task 0

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * Filename: 0-putchar.c
+ * Author: Ahmed-A-T
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
