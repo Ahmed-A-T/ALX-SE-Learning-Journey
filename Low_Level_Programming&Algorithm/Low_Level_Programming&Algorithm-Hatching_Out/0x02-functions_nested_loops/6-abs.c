14bb3c8310808bac78116221d277753b38527a74
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-06-12 21:55:25 +0300

    Complete Task 6

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/*
+ * Filename: 6-abs.c
+ * Author: Ahmed-A-T
+ */
+
+#include "main.h"
+/**
+ * _abs - Compute the absolute value of an integer
+ *
+ * Return: 0;
+ */
+int _abs(int number)
+{
+	if (number < 0)
+		return (number * -1);
+
+	return (0);
+}
