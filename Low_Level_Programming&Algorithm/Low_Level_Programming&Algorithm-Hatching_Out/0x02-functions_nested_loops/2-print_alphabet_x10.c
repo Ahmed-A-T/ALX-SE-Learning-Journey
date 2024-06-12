b4aa2d2a1ba5e50fd43825e1e7024ecb14544bbb
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-06-12 20:47:17 +0300

    Complete Task 2

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/*
+ * Filename: 2-print_alphabet_x10.c
+ * Author: Ahmed-A-T
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
