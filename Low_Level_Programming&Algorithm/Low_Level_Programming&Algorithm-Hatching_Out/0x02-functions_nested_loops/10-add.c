67ba69d28e217f3de20458ce79a791beef1605a6
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-01 20:06:41 +0300

    Complete Task 10

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * Filename: 10-add.c
+ * Author: Ahmed-A-T
+ */
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result.
+ * @num1: The First number to add
+ * @num2: The Second number to add
+ *
+ * Return: return the result of addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
