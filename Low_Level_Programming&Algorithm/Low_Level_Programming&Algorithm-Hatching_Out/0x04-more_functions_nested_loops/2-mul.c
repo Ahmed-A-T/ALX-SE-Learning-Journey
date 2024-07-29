f80e383f38feb9e1acfc7c691539408dd4444a2b
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-29 21:40:28 +0300

    Completing Task 2

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * Filename: 2-mul.c
+ * Author: Ahmed-A-T
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * return: return the multiplied integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
