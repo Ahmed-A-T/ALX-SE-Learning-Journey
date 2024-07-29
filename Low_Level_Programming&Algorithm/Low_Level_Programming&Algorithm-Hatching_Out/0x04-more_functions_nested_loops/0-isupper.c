1965b929dfe14de221d2cde2cb396f8d3cb954a3
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-29 21:28:19 +0300

    Complete Task 0

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/*
+ * Filename: 0-isupper.c
+ * Author: Ahmed-A-T
+ */
+
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: lower or upper case characters that entered by user to check
+ *
+ * Return: 1 if c is uppercase, otherwise return 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (1);
+}
