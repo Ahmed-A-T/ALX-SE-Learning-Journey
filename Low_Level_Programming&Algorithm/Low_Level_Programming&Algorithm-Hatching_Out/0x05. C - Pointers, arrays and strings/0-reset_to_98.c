189b5610fd821ce024d051df6d46e3e5c89f38db
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-10-14 22:22:40 +0300

    Completing Task 0

---
+++ Low_Level_Programming&Algorithm/Low_Level_Programming&Algorithm-Hatching_Out/0x05. C - Pointers, arrays and strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/*
+ * Filename: 0-reset_to_98.c
+ * Author: Ahmed-A-T
+ */
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * @*n: The number that will be updated to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
